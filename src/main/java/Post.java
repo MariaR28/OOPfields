b637f01393049672d0d8cbde32d470399ae9e2f8
Maria R <maria.repina1@gmail.com>
2024-05-20 09:47:39 +0700

    JSON converted

---
+++ src/main/java/Post.java
@@ -0,0 +1,9 @@
+public class Post {
+    public String name;
+    public String passportNumber;
+    public String patronymic;
+    public String phoneNumber;
+    public String surname;
+    public boolean subscription;
+    public FormDate birthday;
+}
